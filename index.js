27c1b2e4d29615d5030da3af19e85a5baf9c3449
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2023-01-18 14:14:54 +0530

    Server Connected to Mongo , lec on 15:51

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.use(bodyParser.json({limit: "30mb" , extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb" , extended: true}))
 
 app.use(cors());
+
+const CONNECTIION_URL = "mongodb+srv://hitaishkumar7587:hitaishkumar456@cluster0.2uk5rys.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTIION_URL)
+.then( () => app.listen(PORT,()=> console.log("Server Running on Port:" ,PORT)) )
+.catch((err) =>console.log('err.message :>> ', err.message) )
+
+// mongoose.set('useFindAndModify',false)  including this line leads to error
